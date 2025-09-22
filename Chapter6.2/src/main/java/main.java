8a5b3db260574e4bf58eba13b6d7ba31afea5b5a
ishtpreet-singh <aulakhishtsingh21@gmail.com>
2025-09-23 00:23:54 +0300

    second commit

---
+++ Chapter6.2/src/main/java/main.java
@@ -0,0 +1,10 @@
+import view.currencyconview;
+import javafx.application.Application;
+
+public class main {
+    public static void main(String[] args) {
+        // This is the correct way to launch a JavaFX application.
+        // It tells JavaFX to start the UI defined in the currencyconview class.
+        Application.launch(currencyconview.class, args);
+    }
+}
